bf6b3273f4b036eac5e0c37d3bd15b4ee82d0e7a
YassWorks <mohamedyassine.kallel@insat.ucar.tn>
2025-09-01 21:03:42 +0100

    Integrate find_references tool into agent configurations

--- app/src/tools/__init__.py
+++ app/src/tools/__init__.py
@@ -1,7 +1,9 @@
 from app.src.tools.file_tools import FILE_TOOLS
 from app.src.tools.exec_tools import EXECUTION_TOOLS
+from app.src.tools.find_tools import FIND_TOOLS
 
 __all__ = [
     "FILE_TOOLS",
     "EXECUTION_TOOLS",
+    "FIND_TOOLS",
 ]
