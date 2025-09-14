4f6e2890b805001b07abc586317d51a41a522c98
YassWorks <mohamedyassine.kallel@insat.ucar.tn>
2025-09-14 20:39:58 +0100

    Remove: Remove unused sentence-transformers dependency

--- app/src/embeddings/hf_embed.py
+++ app/src/embeddings/hf_embed.py
@@ -1,17 +1,17 @@
-from sentence_transformers import SentenceTransformer
+# from sentence_transformers import SentenceTransformer
 
 
-def get_embeddings_hf(model: str, sentences: list[str]) -> list[list[float]]:
-    """
-    Get embeddings for a list of sentences using the Hugging Face SentenceTransformer.
+# def get_embeddings_hf(model: str, sentences: list[str]) -> list[list[float]]:
+#     """
+#     Get embeddings for a list of sentences using the Hugging Face SentenceTransformer.
 
-    Args:
-        sentences (list[str]): List of sentences to embed.
+#     Args:
+#         sentences (list[str]): List of sentences to embed.
 
-    Returns:
-        list[list[float]]: List of embeddings, one for each sentence.
-    """
-    model = SentenceTransformer(model)
-    embeddings = model.encode(sentences)
+#     Returns:
+#         list[list[float]]: List of embeddings, one for each sentence.
+#     """
+#     model = SentenceTransformer(model)
+#     embeddings = model.encode(sentences)
 
-    return embeddings
+#     return embeddings
