25360d7f1f01ae7ccbb69dca57069cd0d82882e3
YassWorks <mohamedyassine.kallel@insat.ucar.tn>
2025-09-22 15:45:36 +0100

    Refactor: Use DataBaseClient singleton instance in handle_embed_request

--- app/src/embeddings/handle_commands.py
+++ app/src/embeddings/handle_commands.py
@@ -1,11 +1,13 @@
-from app.src.cli.cli import DB_CLIENT
+from app.src.embeddings.db_client import DataBaseClient
 from app.src.core.ui import default_ui
 import os
 
 
 def handle_embed_request(*args):
     """Handle the /embed command to embed documents from a specified directory."""
-    if DB_CLIENT is None:
+    db_client = DataBaseClient.get_instance()
+    
+    if db_client is None:
         default_ui.error("Database client is not initialized.")
         return
     
@@ -19,12 +21,4 @@ def handle_embed_request(*args):
     if (directory_path == '.' or directory_path == "./"):
         directory_path = os.getcwd()
     
-    DB_CLIENT.store_documents(directory_path, collection_name)
-
-
-def handle_start_rag_request(*args):
-    raise NotImplementedError("RAG functionality is not yet implemented.")
-
-
-def handle_stop_rag_request(*args):
-    raise NotImplementedError("RAG functionality is not yet implemented.")
+    db_client.store_documents(directory_path, collection_name)
