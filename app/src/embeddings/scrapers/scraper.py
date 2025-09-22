14fb2a0a19bdda4a51e65256efdd7898689f556d
YassWorks <mohamedyassine.kallel@insat.ucar.tn>
2025-09-22 18:45:18 +0100

    Enhance document scraping with Docling

--- app/src/embeddings/scrapers/scraper.py
+++ app/src/embeddings/scrapers/scraper.py
@@ -1,6 +1,7 @@
 from app.src.core.ui import default_ui
-from app.utils.constants import DEFAULT_PATHS
+from app.utils.constants import DEFAULT_PATHS, REGULAR_FILE_EXTENSIONS
 from app.src.helpers.valid_dir import validate_dir_name
+from app.src.embeddings.rag_errors import SetupFailedError, ScrapingFailedError
 import hashlib
 from pathlib import Path
 from docling.document_converter import DocumentConverter
@@ -9,8 +10,8 @@ from docling.datamodel.pipeline_options import PdfPipelineOptions, EasyOcrOption
 from docling.datamodel.pipeline_options import smolvlm_picture_description
 from docling.document_converter import DocumentConverter, PdfFormatOption
 from docling.utils.model_downloader import download_models
+from charset_normalizer import from_path
 import datetime
-import sys
 import os
 
 
@@ -44,16 +45,15 @@ def _setup(path: str = ARTIFACTS_PATH) -> None:
                 progress=False,
                 with_smolvlm=True,
             )
-    except:
-        print("ERROR")
+    except Exception as e:
         default_ui.error(
-            "Failed to download Docling models used for documents parsing. Aborting..."
+            f"Failed to download parsing models: {e}"
         )
-        sys.exit(1)
+        raise
     _SETUP_COMPLETED = True
 
 
-def _get_hash(file_path: str) -> str:
+def get_hash(file_path: str) -> str:
     """Generate SHA-256 hash of a file."""
 
     sha256_hash = hashlib.sha256()
@@ -63,20 +63,44 @@ def _get_hash(file_path: str) -> str:
     return sha256_hash.hexdigest()
 
 
+def _read_regular_file(file_path: str) -> str:
+    try:
+        with open(file_path, "r", encoding="utf-8") as f:
+            return f.read()
+    except UnicodeDecodeError:
+        return str(from_path(file_path).best())
+
+
 def scrape_file(file_path: str):
     """Extract text and metadata from a PDF file."""
+    if any(file_path.lower().endswith(x) for x in REGULAR_FILE_EXTENSIONS):
+        content = _read_regular_file(file_path)
+        return {
+            "content": content,
+            "metadata": {
+                "file_path": Path(file_path).as_posix(),
+                "mod_date": datetime.datetime.fromtimestamp(
+                    Path(file_path).stat().st_mtime
+                ).isoformat(),
+                "hash": get_hash(file_path),
+            },
+        }
 
     global _SETUP_COMPLETED
     if not _SETUP_COMPLETED:
-        _setup()
-    
+        try:
+            _setup()
+        except:
+            raise SetupFailedError()
+
     pipeline_options: PdfPipelineOptions = PdfPipelineOptions(
-        artifacts_path=ARTIFACTS_PATH, do_ocr=True, ocr_options=EasyOcrOptions(force_full_page_ocr=True)
+        artifacts_path=ARTIFACTS_PATH,
+        do_ocr=True,
+        ocr_options=EasyOcrOptions(force_full_page_ocr=True),
     )
 
     pipeline_options.do_formula_enrichment = True
     pipeline_options.do_code_enrichment = True
-    pipeline_options.table_structure_options.do_cell_matching = False
 
     pipeline_options.do_picture_description = True
     pipeline_options.picture_description_options = smolvlm_picture_description
@@ -88,8 +112,8 @@ def scrape_file(file_path: str):
     )
     try:
         doc = doc_converter.convert(file_path).document
-    except Exception:
-        raise
+    except:
+        raise ScrapingFailedError()
 
     return {
         "content": doc.export_to_markdown(),
@@ -98,6 +122,6 @@ def scrape_file(file_path: str):
             "mod_date": datetime.datetime.fromtimestamp(
                 Path(file_path).stat().st_mtime
             ).isoformat(),
-            "hash": _get_hash(file_path),
+            "hash": get_hash(file_path),
         },
     }
