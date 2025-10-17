5d3802dc142396e47f49d5ced66393061190f37b
YassWorks <mohamedyassine.kallel@insat.ucar.tn>
2025-10-17 12:21:17 +0100

    Refactor and enhance Hugging Face embedding function

--- app/src/embeddings/embedding_functions/hf_embed.py
+++ app/src/embeddings/embedding_functions/hf_embed.py
@@ -27,15 +27,15 @@ if not EMBEDDING_MODEL_PATH:
 
 
 class HFEmbedder:
-    
+
     def __init__(self, model_name: str):
         self.model_name = model_name
-    
+
     # Mean Pooling - Take attention mask into account for correct averaging
     @staticmethod
     def _mean_pooling(model_output, attention_mask):
         import torch
-        
+
         token_embeddings = model_output[
             0
         ]  # First element of model_output contains all token embeddings
@@ -46,33 +46,42 @@ class HFEmbedder:
             input_mask_expanded.sum(1), min=1e-9
         )
 
-
-    def get_embeddings(self, sentences: list[str] | str) -> list[list[float]]:  
+    def get_embeddings(self, sentences: list[str] | str) -> list[list[float]]:
         """
         Get embeddings for a list of sentences using a Hugging Face model.
+
         Args:
             sentences (list[str] | str): List of sentences to embed or a single sentence.
-            model_name (str): Name of the Hugging Face model to use.
 
         Returns:
             list[list[float]]: List of embeddings for each sentence.
         """
         global _DISPLAYED_DOWNLOADING_MESSAGE
-        
+
         from transformers import AutoTokenizer, AutoModel
         import torch.nn.functional as F
         import torch
         import transformers.utils.logging as hf_logging
-        
+
         hf_logging.set_verbosity_error()
+        os.makedirs(EMBEDDING_MODEL_PATH, exist_ok=True)
+
         if not _DISPLAYED_DOWNLOADING_MESSAGE and not EMBEDDING_MODEL_PATH.exists():
             with default_ui.console.status("Downloading embedding model..."):
-                tokenizer = AutoTokenizer.from_pretrained(self.model_name, cache_dir=EMBEDDING_MODEL_PATH)
-                model = AutoModel.from_pretrained(self.model_name, cache_dir=EMBEDDING_MODEL_PATH)
+                tokenizer = AutoTokenizer.from_pretrained(
+                    self.model_name, cache_dir=EMBEDDING_MODEL_PATH
+                )
+                model = AutoModel.from_pretrained(
+                    self.model_name, cache_dir=EMBEDDING_MODEL_PATH
+                )
             _DISPLAYED_DOWNLOADING_MESSAGE = True
         else:
-            tokenizer = AutoTokenizer.from_pretrained(self.model_name, cache_dir=EMBEDDING_MODEL_PATH)
-            model = AutoModel.from_pretrained(self.model_name, cache_dir=EMBEDDING_MODEL_PATH)
+            tokenizer = AutoTokenizer.from_pretrained(
+                self.model_name, cache_dir=EMBEDDING_MODEL_PATH
+            )
+            model = AutoModel.from_pretrained(
+                self.model_name, cache_dir=EMBEDDING_MODEL_PATH
+            )
 
         encoded_input = tokenizer(
             sentences, padding=True, truncation=True, return_tensors="pt"
@@ -81,7 +90,9 @@ class HFEmbedder:
         with torch.no_grad():
             model_output = model(**encoded_input)
 
-        sentence_embeddings = self._mean_pooling(model_output, encoded_input["attention_mask"])
+        sentence_embeddings = self._mean_pooling(
+            model_output, encoded_input["attention_mask"]
+        )
         sentence_embeddings = F.normalize(sentence_embeddings, p=2, dim=1)
 
         return sentence_embeddings.tolist()
